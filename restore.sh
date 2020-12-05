257128b7de3639b19e642865a39ef4e76f0f3581
lalaalal <bontin0128@gmail.com>
2020-12-05 23:54:44 +0900

    restore

--- restore.sh
+++ restore.sh
@@ -8,6 +8,5 @@ if [ -z $1 ] || [ "local" = "$1" ]; then
 	rsync -arv --files-from=list.txt . ~/
 	rsync -rv bin/ /usr/local/bin/
 elif [ "server" = "$1" ]; then
-	rsync -arv --files-from=list.txt note3:~/$HACKUP_PATH ./
-	rsync -rv note3:~/$BACKUP_PATH/bin/ ./bin/
+	rsync -azzv note3:~/$BACKUP_PATH .
 fi
