0a9417e82e024a41607637eca3efeb5695498144
lalaalal <bontin0128@gmail.com>
2021-03-03 23:32:30 +0900

    .

--- backup.sh
+++ backup.sh
@@ -6,9 +6,9 @@ BACKUP_PATH=backup
 
 if [ -z $1 ] || [ "local" = "$1" ]; then
 	rsync --delete -arv --files-from=list.txt ~/ .
-	rsync /usr/local/bin/ bin/
+	rsync --delete -rv /usr/local/bin/ bin/
 else
 	rsync list.txt $1:~/$BACKUP_PATH/
 	rsync --delete -arv --files-from=list.txt ~/ $1:~/$BACKUP_PATH
-	rsync -rv /usr/local/bin/ $1:~/$BACKUP_PATH/bin/
+	rsync --delete -rv /usr/local/bin/ $1:~/$BACKUP_PATH/bin/
 fi
--- restore.sh
+++ restore.sh
@@ -6,10 +6,10 @@ BACKUP_PATH=backup
 
 if [ -z $1 ] || [ "local" = "$1" ]; then
 	rsync -arv --delete --files-from=list.txt . ~/
-	rsync -rv bin/ /usr/local/bin/
+	rsync -rv --delete bin/ /usr/local/bin/
 else
 	rsync $1:~/$BACKUP_PATH/list.txt .
 	rsync -avr --delete --files-from=list.txt $1:~/$BACKUP_PATH ~/
-	rsync -rv $1:~/$BACKUP_PATH/bin/ /usr/local/bin/
+	rsync -rv --delete $1:~/$BACKUP_PATH/bin/ /usr/local/bin/
 #	rsync -azzv $1:~/$BACKUP_PATH/ ./
 fi
